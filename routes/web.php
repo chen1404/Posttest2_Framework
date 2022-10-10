831bf73856e626aadbb21156d75b71e6ee93aaeb
Satria Bagus Eka Candra <satriabaguse@gmail.com>
2022-10-10 14:55:29 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Broker;
+use App\Models\Saham;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index', [
+        "sahams" => Saham::all(),
+        "brokers" => Broker::all()
+    ]);
+})->name('index');
+
+Route::get('/details', function () {
+    return view('details', [
+        "sahams" => Saham::all(),
+        "brokers" => Broker::all()
+    ]);
+});
